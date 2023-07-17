89dd75918d1d14c6f5138d2c2f224ff38fae49ff
Ian <ianseabock@gmail.com>
2023-07-17 13:39:28 -0700

    Typewriter

--- components/Typewriter.jsx
+++ components/Typewriter.jsx
@@ -1,9 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
+const BlinkingCursor = () => {
+    const [show, setShow] = useState(true);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setShow((prevShow) => !prevShow);
+        }, 500);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
+    return <div sx={{ width: '1rem' }}>{show ? '_' : ' '}</div>;
+};
 const Typewriter = ({ sentence, typingSpeed }) => {
     const [currentText, setCurrentText] = useState('');
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [fontSize, setFontSize] = useState(16);
+    let speed = Math.floor(Math.random() * typingSpeed) + 50;
+
+    useEffect(() => {
+        var container = document.getElementById('sentenceTest');
+        var text = document.getElementById('text');
+        var fontSize = 20;
+        text.style.fontSize = fontSize + 'px';
+        while (Math.ceil(text.clientWidth) > container.clientWidth) {
+            fontSize--;
+            text.style.fontSize = fontSize + 'px';
+            setFontSize(`${fontSize--}px`);
+        }
+    }, [fontSize]);
 
     useEffect(() => {
         const timer = setInterval(() => {
@@ -13,14 +44,43 @@ const Typewriter = ({ sentence, typingSpeed }) => {
             } else {
                 clearInterval(timer);
             }
-        }, typingSpeed);
+        }, speed);
 
         return () => {
             clearInterval(timer);
         };
     }, [sentence, currentIndex, typingSpeed]);
 
-    return <span>{currentText}</span>;
+    return (
+        <Box flexDirection="column" alignItems="center" justifyContent="center">
+            <Typography
+                component="div"
+                sx={{
+                    width: '24.2rem',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'flex-start',
+                    margin: '0 auto',
+                    fontSize: fontSize
+                }}>
+                {currentText}
+                <BlinkingCursor />
+            </Typography>
+            <div id="sentenceTest" style={{ width: '24.2rem' }}>
+                <div
+                    id="text"
+                    style={{
+                        whiteSpace: 'nowrap',
+                        width: 'auto',
+                        position: 'absolute',
+                        color: 'transparent'
+                    }}>
+                    {sentence}
+                </div>
+            </div>
+        </Box>
+    );
 };
 
 Typewriter.propTypes = {
